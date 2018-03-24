0192f1a3584a6d72574991df2909cf0533352af8
Oleg Dolya <oleg.dolya@gmail.com>
2018-03-24 09:39:24 +0200

    add SendDB fuction

---
+++ pusher/database.go
@@ -0,0 +1,33 @@
+package pusher
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/qor/notification"
+)
+
+// SendDB - save message to database
+func SendDB(message *notification.Message, db * gorm.DB) error {
+	notice := notification.QorNotification{
+		Title: message.Title,
+		Body:  message.Body,
+		// Val: message.Val,
+		MessageType: message.MessageType,
+		// ResolvedAt:  message.ResolvedAt,
+	}
+
+	if len(message.From.(string)) > 0 {
+		notice.From = message.From.(string)
+	} else {
+		notice.From = "system"
+	}
+	if len(message.To.(string)) > 0 {
+		notice.From = message.To.(string)
+	} else {
+		notice.From = "all"
+	}
+	if len(message.Link) > 0 {
+		notice.Link = message.Link
+	}
+
+	return db.Debug().Save(&notice).Error
+}
